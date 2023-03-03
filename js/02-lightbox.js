62f37698ef7209cfd67e096039a15a583d5bf5d1
hannakaczynska <hannakacz13@gmail.com>
2023-03-03 11:30:18 +0100

    added 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryGrid = galleryItems
+  .map(
+    item =>
+      `<div class="gallery__item">
+      <a class="gallery__link" href=${item.original}>
+      <img class="gallery__image" src=${item.preview} data-source=${item.original} alt=${item.description}/>
+      </a>
+      </div>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryGrid);
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: '250ms',
+  });
+}
